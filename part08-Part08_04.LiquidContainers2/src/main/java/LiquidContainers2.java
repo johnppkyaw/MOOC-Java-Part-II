0e5469d0a0c5276d97e2cae7ebf4d64c1df0dc60
John Kyaw <jjpyae@gmail.com>
2024-03-26 10:02:29 -0700

    Part 8, Exercise 4 complete

--- part08-Part08_04.LiquidContainers2/src/main/java/LiquidContainers2.java
+++ part08-Part08_04.LiquidContainers2/src/main/java/LiquidContainers2.java
@@ -3,39 +3,45 @@ import java.util.Scanner;
 
 public class LiquidContainers2 {
 
-    public class Container {
-        private int amount;
-        public Container(){
-
-        }
-        public int contains() {
-            return this.amount;
-        }
-        public void add(int amount) {
-            if(amount > 0 || this.amount < 100) {
-                if(this.amount + amount < 100) {
-                    this.amount += amount;
-                } else {
-                    this.amount = 100;
-                }                
-            }
-        }
-        public 
-    }
-
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
+        Container container1 = new Container();
+        Container container2 = new Container();
 
         while (true) {
+            
             System.out.print("> ");
+            System.out.println("First: " + container1);
+            System.out.print("> ");
+            System.out.println("Second: " + container2);
+
 
             String input = scan.nextLine();
             if (input.equals("quit")) {
                 break;
             }
 
+            String[] commandList = input.split(" ");
+            String command = commandList[0];
+            int number = Integer.parseInt(commandList[1]); 
+            if (command.equals("add")) {
+                container1.add(number);                
+            }
+            if (command.equals("remove")) {
+                container2.remove(number);                
+            }
+            if (command.equals("move")) {
+                int container1Amt = container1.contains();
+                if(container1Amt > number) {
+                    container1.remove(number);
+                    container2.add(number); 
+                } else {
+                    container1.remove(number);
+                    container2.add(container1Amt); 
+                }             
+            }
         }
+        scan.close();
     }
 
 }
