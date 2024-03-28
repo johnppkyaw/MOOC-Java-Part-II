3cc157ac104dc10ab72ee21e400073e599fb0ef2
John Kyaw <jjpyae@gmail.com>
2024-03-27 18:27:11 -0700

    Complete part 08 - 09 Print Me another hash map

--- part08-Part08_09.PrintMeAnotherHashmap/src/main/java/Program.java
+++ part08-Part08_09.PrintMeAnotherHashmap/src/main/java/Program.java
@@ -2,6 +2,20 @@
 import java.util.HashMap;
 
 public class Program {
+    public static void printValues(HashMap<String, Book> hashmap) {
+        for(Book book : hashmap.values()) {
+            System.out.println(book);
+        }
+    }
+
+    public static void printValueIfNameContains(HashMap<String, Book> hashmap, String text) {
+        for(Book book : hashmap.values()) {
+            String bookName = book.getName();
+            if(bookName.contains(text)) {
+                System.out.println(book);
+            }
+        }
+    }
 
     public static void main(String[] args) {
         // Test your program here!
